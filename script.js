0eb7f6b4125ef6797d2e655631ba4d1e309f5176
basnetrajpradip <basnetrajpradip@gmail.com>
2023-03-24 12:22:38 +0545

    Add required containers and grids through DOM and html

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const container = document.querySelector(".container");
-const row1 = document.querySelector(".row1");
+const row = [];
+const grid = [];
+const num = 4;
+/* const row1 = document.querySelector(".row1");
 const row2 = document.querySelector(".row2");
 const row3 = document.querySelector(".row3");
-const row4 = document.querySelector(".row4");
-
-const grid = [];
+const row4 = document.querySelector(".row4"); */
+/* 
 for (let i = 0; i < 16; i++) {
   grid[i] = document.createElement("div");
   grid[i].classList.add("grids");
@@ -16,5 +18,19 @@ for (let i = 0; i < 16; i++) {
     row3.appendChild(grid[i]);
   } else {
     row4.appendChild(grid[i]);
+  }
+}
+ */
+for (let i = 0; i < num; i++) {
+  row[i] = document.createElement("div");
+  row[i].classList.add(`row${i}`);
+  container.appendChild(row[i]);
+}
+
+for (let i = 0; i < num; i++) {
+  for (let j = 0; j < num; j++) {
+    grid[i] = document.createElement("div");
+    grid[i].classList.add("grid");
+    row[i].appendChild(grid[i]);
   }
 }
