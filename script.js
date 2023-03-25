b0900552954848622d67324526d9b2357251f7f7
basnetrajpradip <basnetrajpradip@gmail.com>
2023-03-25 14:18:02 +0545

    Add grid to document

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 const container = document.querySelector(".container");
-const row = [];
-const grid = [];
-const num = 4;
-/* const row1 = document.querySelector(".row1");
-const row2 = document.querySelector(".row2");
-const row3 = document.querySelector(".row3");
-const row4 = document.querySelector(".row4"); */
-/* 
-for (let i = 0; i < 16; i++) {
-  grid[i] = document.createElement("div");
-  grid[i].classList.add("grids");
-  if (i < 4) {
-    row1.appendChild(grid[i]);
-  } else if (i < 8 && i >= 4) {
-    row2.appendChild(grid[i]);
-  } else if (i < 12 && i >= 8) {
-    row3.appendChild(grid[i]);
-  } else {
-    row4.appendChild(grid[i]);
-  }
-}
- */
+const rows = [];
+const grids = [];
+const slider = document.querySelector(".slider");
+const value = document.querySelector(".size-value");
+let num = slider.value;
+value.textContent = slider.value;
+
 for (let i = 0; i < num; i++) {
-  row[i] = document.createElement("div");
-  row[i].classList.add(`row${i}`);
-  container.appendChild(row[i]);
+  rows[i] = document.createElement("div");
+  rows[i].classList.add(`row`);
+  container.appendChild(rows[i]);
 }
 
 for (let i = 0; i < num; i++) {
   for (let j = 0; j < num; j++) {
-    grid[i] = document.createElement("div");
-    grid[i].classList.add("grid");
-    row[i].appendChild(grid[i]);
+    grids[i] = document.createElement("div");
+    grids[i].classList.add("grids");
+    rows[i].appendChild(grids[i]);
   }
 }
+
+slider.addEventListener("input", (e) => {
+  value.textContent = slider.value;
+  num = slider.value;
+});
+
+const grid = document.querySelectorAll(".grids");
+grid.forEach((item) => {
+  item.addEventListener("mouseover", (e) => {
+    item.classList.add("bg-color");
+  });
+});
